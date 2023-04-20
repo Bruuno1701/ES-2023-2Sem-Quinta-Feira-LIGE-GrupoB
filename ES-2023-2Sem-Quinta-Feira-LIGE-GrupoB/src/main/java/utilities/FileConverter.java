2bbb1c187d77e760c4c26926ac1515769a0e81cb
bcbsf-iscte <125586795+bcbsf-iscte@users.noreply.github.com>
2023-04-20 08:28:12 +0100

    testes

--- ES-2023-2Sem-Quinta-Feira-LIGE-GrupoB/src/main/java/utilities/FileConverter.java
+++ ES-2023-2Sem-Quinta-Feira-LIGE-GrupoB/src/main/java/utilities/FileConverter.java
@@ -1,6 +1,7 @@
 package utilities;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
@@ -13,6 +14,7 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
@@ -89,7 +91,8 @@ public class FileConverter
     public static File jsonTocsv(String jsonPath, String csvPath)
     {
 	File f = new File(jsonPath);
-	    
+//	System.out.println(f.exists());
+	   
 	if (!FilenameUtils.getExtension(jsonPath).equals("json"))
 		    return f;
 	    
@@ -103,8 +106,9 @@ public class FileConverter
 	    JSONArray documento = new JSONArray(jsonString);
 
 	    List<String> estrutura = new ArrayList<String>();
+	    estrutura.add(new String());
 
-	    String cabecalho = ("Curso, Unidade Curricular, Turno, Turma, Inscritos no Turno, Dia da Semana, Hora de Inicio da Aula, Hora Final da Aula, Data da Aula, Sala Atribuida, Lotacao da Sala");
+	    String cabecalho = ("Curso;Unidade Curricular;Turno;Turma;Inscritos no turno;Dia da semana;Hora início da aula;Hora fim da aula;Data da aula;Sala atribuída à aula;Lotação da sala");
 	    estrutura.add(cabecalho);
 
 	    for (int i = 0; i < documento.length(); i++)
@@ -123,14 +127,14 @@ public class FileConverter
 		String salaAtribuida = object.optString("Sala atribuída à aula", "");
 		String lotacaoDaSala = object.optString("Lotação da sala", "");
 
-		String csvString = String.format("%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s", curso, unidadeCurricular,
+		String csvString = String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s", curso, unidadeCurricular,
 			turno, turma, inscritosNoTurno, diaDaSemana, horaInicioAula, horaFimAula, dataAula,
 			salaAtribuida, lotacaoDaSala);
 		estrutura.add(csvString);
 	    }
 
-	    file = new File(csvPath, "horario_exemplo_convertido.csv");
-	    FileUtils.writeLines(file, estrutura);
+	    file = new File(csvPath);
+	    FileUtils.writeStringToFile(file, StringUtils.join(estrutura, "\n"), "UTF-8");   
 	} catch (Exception e)
 	{
 	    e.printStackTrace();
--- ES-2023-2Sem-Quinta-Feira-LIGE-GrupoB/src/test/java/utilities/FileConverterTest.java
+++ ES-2023-2Sem-Quinta-Feira-LIGE-GrupoB/src/test/java/utilities/FileConverterTest.java
@@ -10,31 +10,30 @@ import java.nio.file.Paths;
 
 import org.junit.Test;
 
-public class FileConverterTest
-{
+public class FileConverterTest {
 
-    @Test
-    public void testcsvTojson() throws IOException
-    {
-	Path convertedJsonPath = Paths.get(FileConverter.csvTojson("src/test/resources/horario_exemplo.csv",
-		"src/test/resources/horario_exemplo_convertido.json").getAbsolutePath());
-	Path jsonPath = Paths.get("src/test/resources/horario_exemplo.json");
-	System.out.println(Files.mismatch(convertedJsonPath, jsonPath));
-	assert (Files.mismatch(convertedJsonPath, jsonPath) == -1);
-	convertedJsonPath = Paths.get(FileConverter
-		.csvTojson("src/test/resources/horario_exemplo.json", "src/test/resources/horario_exemplo.json")
-		.getAbsolutePath());
-	assert (Files.mismatch(convertedJsonPath, jsonPath) == -1);
-    }
+	@Test
+	public void testcsvTojson() throws IOException {
+		Path convertedJsonPath = Paths.get(FileConverter.csvTojson("src/test/resources/horario_exemplo.csv",
+				"src/test/resources/horario_exemplo_convertido.json").getAbsolutePath());
+		Path jsonPath = Paths.get("src/test/resources/horario_exemplo.json");
+		System.out.println(Files.mismatch(convertedJsonPath, jsonPath));
+		assert (Files.mismatch(convertedJsonPath, jsonPath) == -1);
+		convertedJsonPath = Paths.get(FileConverter
+				.csvTojson("src/test/resources/horario_exemplo.json", "src/test/resources/horario_exemplo.json")
+				.getAbsolutePath());
+		assert (Files.mismatch(convertedJsonPath, jsonPath) == -1);
+	}
 
-    @Test
-    public void testjsonTocsv() throws IOException
-    {
-	Path convertedCsvPath = Paths.get(FileConverter.jsonTocsv("scr/test/resources/horario_exemplo.json", "src/test/resources/horario_exemplo_convertido.csv").getAbsolutePath());
+	@Test
+	public void testjsonTocsv() throws IOException {
+		Path convertedCsvPath = Paths.get(FileConverter.jsonTocsv("src/test/resources/horario_exemplo.json",
+				"src/test/resources/horario_exemplo_convertido.csv").getAbsolutePath());
 		Path csvPath = Paths.get("src/test/resources/horario_exemplo.csv");
 		System.out.println(Files.mismatch(convertedCsvPath, csvPath));
-		assert(Files.mismatch(convertedCsvPath, csvPath)==-1);
-		convertedCsvPath = Paths.get(FileConverter.jsonTocsv("scr/test/resources/horario_exemplo.json", "src/test/resources/horario_exemplo_convertido.csv").getAbsolutePath());
-		assert(Files.mismatch(convertedCsvPath, csvPath)==-1);
-    }
+		assert (Files.mismatch(convertedCsvPath, csvPath) == -1);
+		convertedCsvPath = Paths.get(FileConverter.jsonTocsv("src/test/resources/horario_exemplo.json",
+				"src/test/resources/horario_exemplo_convertido.csv").getAbsolutePath());
+		assert (Files.mismatch(convertedCsvPath, csvPath) == -1);
+	}
 }
