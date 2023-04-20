8fc58706cab997b7dcc7c577bb59071ddeb6a076
bcbsf-iscte <125586795+bcbsf-iscte@users.noreply.github.com>
2023-04-20 11:57:53 +0100

    working

--- ES-2023-2Sem-Quinta-Feira-LIGE-GrupoB/src/test/java/utilities/FileConverterTest.java
+++ ES-2023-2Sem-Quinta-Feira-LIGE-GrupoB/src/test/java/utilities/FileConverterTest.java
@@ -1,13 +1,14 @@
 package utilities;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 public class FileConverterTest {
@@ -17,12 +18,13 @@ public class FileConverterTest {
 		Path convertedJsonPath = Paths.get(FileConverter.csvTojson("src/test/resources/horario_exemplo.csv",
 				"src/test/resources/horario_exemplo_convertido.json").getAbsolutePath());
 		Path jsonPath = Paths.get("src/test/resources/horario_exemplo.json");
-		System.out.println(Files.mismatch(convertedJsonPath, jsonPath));
-		assert (Files.mismatch(convertedJsonPath, jsonPath) == -1);
-		convertedJsonPath = Paths.get(FileConverter
-				.csvTojson("src/test/resources/horario_exemplo.json", "src/test/resources/horario_exemplo.json")
-				.getAbsolutePath());
-		assert (Files.mismatch(convertedJsonPath, jsonPath) == -1);
+		BufferedReader bf1 = new BufferedReader(new FileReader(convertedJsonPath.toFile()));
+		BufferedReader bf2 = new BufferedReader(new FileReader(jsonPath.toFile()));
+		
+		assertTrue(IOUtils.contentEqualsIgnoreEOL(bf1, bf2));
+		convertedJsonPath = Paths.get(FileConverter.csvTojson("src/test/resources/horario_exemplo.json",
+				"src/test/resources/horario_exemplo_convertido.json").getPath());
+		assertTrue(convertedJsonPath.equals(jsonPath));
 	}
 
 	@Test
@@ -30,10 +32,15 @@ public class FileConverterTest {
 		Path convertedCsvPath = Paths.get(FileConverter.jsonTocsv("src/test/resources/horario_exemplo.json",
 				"src/test/resources/horario_exemplo_convertido.csv").getAbsolutePath());
 		Path csvPath = Paths.get("src/test/resources/horario_exemplo.csv");
-		System.out.println(Files.mismatch(convertedCsvPath, csvPath));
-		assert (Files.mismatch(convertedCsvPath, csvPath) == -1);
-		convertedCsvPath = Paths.get(FileConverter.jsonTocsv("src/test/resources/horario_exemplo.json",
-				"src/test/resources/horario_exemplo_convertido.csv").getAbsolutePath());
-		assert (Files.mismatch(convertedCsvPath, csvPath) == -1);
+
+		BufferedReader bf1 = new BufferedReader(new FileReader(convertedCsvPath.toFile()));
+		BufferedReader bf2 = new BufferedReader(new FileReader(csvPath.toFile()));
+
+		assertTrue(IOUtils.contentEqualsIgnoreEOL(bf1, bf2));
+		
+		convertedCsvPath = Paths.get(FileConverter.jsonTocsv("src/test/resources/horario_exemplo.csv",
+				"src/test/resources/horario_exemplo_convertido.csv").getPath());
+		assertTrue(convertedCsvPath.equals(csvPath));
+		
 	}
 }
