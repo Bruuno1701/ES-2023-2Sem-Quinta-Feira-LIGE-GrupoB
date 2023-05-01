fab28a64df6d35416706f6419d6a7fb90b2c1a6b
DeboraNovo <dimgg@iscte-iul.pt>
2023-05-01 16:23:47 +0100

    Download timetable button on HTML & JSON Conf file
    Botão de download dos horários e criação de um ficheiro de configuração JSON para permitir ao user escolher qual a pasta onde descarregar o horário em csv ou json.

--- ES-2023-2Sem-Quinta-Feira-LIGE-GrupoB/src/main/java/TimeTable.java
+++ ES-2023-2Sem-Quinta-Feira-LIGE-GrupoB/src/main/java/TimeTable.java
@@ -1,24 +1,212 @@
 import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.io.FileUtils;
+import java.io.FileInputStream;
+import java.io.DataOutputStream;
+
 
 import utilities.FileConverter;
 
-public class TimeTable {
-	private File file;
+public class TimeTable{
 	
-	public TimeTable(String path) {
-		this.file = findFile(path);
-	}
+    private File file;
 
-	private File findFile(String path) {
-		// TODO Auto-generated method stub
-		return null;
+    /**
+     * @author pedro
+     * Construtor da class recebe uma string path para carregar o ficheiro 
+     * 
+     * @param path caminho (path) para o ficheiro pretendido
+     */
+    public TimeTable(String path)
+    {
+	try
+	{
+	    this.file = findFile(path, null);
+	} catch (IOException e)
+	{
+	    e.printStackTrace();
+	    System.out.println("Erro ao criar a TimeTable");
 	}
-	
-	public void saveAsJSON(String path) {
-		FileConverter.CSVtoJSON(file, path);
+    }
+    
+    /**
+     * @author pedro
+     * Construtor da class recebe uma string path para carregar o ficheiro e 
+     * uma string para guardar o ficheiro
+     * 
+     * @param path caminho para o ficheiro pretendido, pode ser um diret�rio ou URL
+     * @param localDirectory caminho onde vai ser guardado o ficheiro 
+     */
+    public TimeTable(String path, String localDirectory)
+    {
+	try
+	{
+	    this.file = findFile(path, localDirectory);
+	} catch (IOException e)
+	{
+	    e.printStackTrace();
+	    System.out.println("Erro ao criar a TimeTable");
 	}
-	
-	public void saveAsCSV(String path) {
-		FileConverter.JSONtoCSV(file, path);
+    }
+    
+    /**
+     * @author pedro
+     * Se o par�metro path for um path vai carregar esse ficheiro para a vari�vel local file, caso o path
+     * seja um URL vai carregar o ficheiro do URL e salva-o no path localDirectory
+     *
+     * @param path caminho para o ficheiro pretendido, pode ser diret�rio ou URL
+     * @param localDirectory caminho onde vai ser guardado o ficheiro 
+     *
+     * @return o file que se carregou
+     */
+    private File findFile(String path, String localDirectory) throws IOException
+    {
+	File file = null;
+	if (isURL(path))
+	{
+		if(localDirectory == null) 
+		{
+			System.out.println("erro path url e localdirectory null");
+			return file;
+		}
+		return downloadFile(path,localDirectory);
 	}
+	else
+	{
+		System.out.println("n�o url");
+	    file = new File(path);
+	}
+	return file;
+    }
+    
+    /**
+     * @author filipa
+     * Atualiza o ficheiro da class para a convers�o do mesmo no formato JSON
+     *
+     * @param path diret�rio onde se pretende guardar o ficheiro que vai ser criado ao invocar o convertor
+     *
+     * @return void
+     */
+    public void saveAsJSON(String path)
+    {
+	try
+	{
+		this.file =FileConverter.csvTojson(file.getAbsolutePath(), path);
+	}catch (IOException e)
+	{
+	    e.printStackTrace();
+	}
+    }
+
+    /**
+     * @author bruna
+     * Atualiza o ficheiro da class para a convers�o do mesmo no formato CSV
+     *
+     * @param path diret�rio onde se pretende guardar o ficheiro que vai ser criado ao invocar o convertor
+     *
+     * @return void
+     */
+    public void saveAsCSV(String path)
+    {
+    	this.file = FileConverter.jsonTocsv(file.getAbsolutePath(), path);
+    }
+
+    /**
+     * @author pedro
+     * Guarda o ficheiro local num local passado
+     *
+     * @param Path string que pode ser um URL ou um path onde se prentede guardar o ficheiro da class
+     *
+     * @return void
+     * @throws IOException
+     */
+    public void saveFile(String Path) throws IOException
+    {
+	if (!isURL(Path))
+	{
+	    File directory = new File(Path);
+	    if (!directory.exists())
+	    {
+		System.err.println("Diretoria n�o existe");
+		return;
+	    }
+	    FileUtils.copyFileToDirectory(this.file, directory);
+	}
+	else
+	{
+	    URL url = new URL(Path);
+	    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+	    connection.setDoOutput(true);
+	    connection.setRequestMethod("POST");
+	    connection.setRequestProperty("Content-Type", "multipart/form-data");
+	    DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+	    FileInputStream fileInputStream = new FileInputStream(this.file);
+	    byte[] buffer = new byte[1024];
+	    int bytesRead;
+	    while ((bytesRead = fileInputStream.read(buffer)) != -1)
+	    {
+		outputStream.write(buffer, 0, bytesRead);
+	    }
+	    fileInputStream.close();
+	    outputStream.flush();
+	    outputStream.close();
+	    int responseCode = connection.getResponseCode();
+	    System.out.println("Response Code : " + responseCode);
+	}
+    }
+
+    /**
+     * @author pedro
+     * verifica se a string � um URL
+     *
+     * @param url URL do ficheiro pretendido
+     *
+     * @return true se o parametro for um URL e false caso contr�rio
+     */
+    public boolean isURL(String url)
+    {
+	String regex = "^(https?|ftp|file)://.+";
+	Pattern pattern = Pattern.compile(regex);
+	Matcher matcher = pattern.matcher(url);
+	return matcher.matches();
+    }
+    
+    /**
+     * @author pedro
+     * Faz download de um ficheiro atrav�s de um url para uma diretoria
+     *
+     * @param url URL do ficheiro pretendido
+     * @param directory directoria onde o ficheiro vai ser guardado
+     *
+     * @return o ficheiro guardado
+     * @throws IOException
+     */
+    public File downloadFile(String url, String directory) throws IOException 
+    {
+        URL fileUrl = new URL(url);
+        String fileName = fileUrl.getFile().substring(fileUrl.getFile().lastIndexOf('/') + 1);
+        File destinationFile = new File(directory + "/" + fileName);
+        FileUtils.copyURLToFile(fileUrl, destinationFile);
+        String fileContent = FileUtils.readFileToString(destinationFile);
+        fileContent = fileContent.replaceFirst("^\\s+", "");
+        FileUtils.writeStringToFile(destinationFile, fileContent, StandardCharsets.UTF_8);
+        
+        return destinationFile;
+    }
+    
+    /**
+     * @author pedro
+     * Devolve o ficheiro guardado na vari�vel local file.
+     *
+     * @return o ficheiro this.file
+     */
+    public File getFile() {
+    	return this.file;
+    }
+    
 }
--- ES-2023-2Sem-Quinta-Feira-LIGE-GrupoB/src/main/java/utilities/FileConverter.java
+++ ES-2023-2Sem-Quinta-Feira-LIGE-GrupoB/src/main/java/utilities/FileConverter.java
@@ -1,13 +1,128 @@
 package utilities;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class FileConverter {
-	public static File CSVtoJSON(File f, String path) {
-		return null;
+
+	/**
+	 * @author filipa Devolve um ficheiro json resultante da conversão de um
+	 *         ficheiro csv.
+	 *
+	 * @param csvPath  caminho para o ficheiro csv a ser convertido
+	 * @param jsonPath caminho onde o ficheiro json resultante da conversão deve ser
+	 *                 guardado
+	 * @return o ficheiro json resultante da conversão
+	 * @throws IOException
+	 */
+	public static File csvTojson(String csvPath, String jsonPath) throws IOException {
+		File csvFile = new File(csvPath);
+		System.out.println(FilenameUtils.getExtension(csvPath));
+		if (!FilenameUtils.getExtension(csvPath).equals("csv"))
+			return csvFile;
+
+		File jsonFile = new File(jsonPath);
+		PrintWriter writer = new PrintWriter(jsonFile, Charset.forName("UTF-8"));
+
+		CSVParser csvParser = CSVParser.parse(csvFile, Charset.forName("UTF-8"),
+				CSVFormat.DEFAULT.withFirstRecordAsHeader().builder().setDelimiter(';').build());
+		List<CSVRecord> records = csvParser.getRecords();
+		List<String> headers = csvParser.getHeaderNames();
+		writer.println("[");
+		for (int i = 0; i < records.size(); i++) {
+			CSVRecord record = records.get(i);
+			writer.println(" {");
+			List<String> list = record.toList();
+			list.removeAll(Arrays.asList("", null));
+			for (int j = 0; j < list.size(); j++) {
+				String value = list.get(j);
+				String header = headers.get(j);
+				if (j != list.size() - 1) {
+					writer.println("   \"" + header + "\": \"" + value + "\",");
+				} else {
+					writer.println("   \"" + header + "\": \"" + value + "\"");
+				}
+			}
+			if (i != records.size() - 1) {
+				writer.println(" },");
+			} else {
+				writer.println(" }");
+			}
+		}
+
+		writer.print("]");
+		try {
+			return jsonFile;
+		} finally {
+			writer.close();
+			csvParser.close();
+		}
 	}
-	
-	public static File JSONtoCSV(File f, String path) {
-		return null;
+
+	public static File jsonTocsv(String jsonPath, String csvPath) {
+		File f = new File(jsonPath);
+
+		if (!FilenameUtils.getExtension(jsonPath).equals("json")) {
+			return f;
+		}
+
+		String jsonString;
+		File file = null;
+
+		try {
+			jsonString = FileUtils.readFileToString(f, "UTF-8");
+
+			JSONArray documento = new JSONArray(jsonString);
+
+			List<String> estrutura = new ArrayList<String>();
+			estrutura.add(new String());
+
+			String cabecalho = ("Curso;Unidade Curricular;Turno;Turma;Inscritos no turno;Dia da semana;Hora início da aula;Hora fim da aula;Data da aula;Sala atribuída à aula;Lotação da sala");
+			estrutura.add(cabecalho);
+
+			for (int i = 0; i < documento.length(); i++) {
+				JSONObject object = documento.getJSONObject(i);
+
+				String curso = object.optString("Curso", "");
+				String unidadeCurricular = object.optString("Unidade Curricular", "");
+				String turno = object.optString("Turno", "");
+				String turma = object.optString("Turma", "");
+				String inscritosNoTurno = object.optString("Inscritos no turno", "");
+				String diaDaSemana = object.optString("Dia da semana", "");
+				String horaInicioAula = object.optString("Hora início da aula", "");
+				String horaFimAula = object.optString("Hora fim da aula", "");
+				String dataAula = object.optString("Data da aula", "");
+				String salaAtribuida = object.optString("Sala atribuída à aula", "");
+				String lotacaoDaSala = object.optString("Lotação da sala", "");
+
+				String csvString = String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s", curso, unidadeCurricular, turno,
+						turma, inscritosNoTurno, diaDaSemana, horaInicioAula, horaFimAula, dataAula, salaAtribuida,
+						lotacaoDaSala);
+				estrutura.add(csvString);
+			}
+
+			file = new File(csvPath);
+			FileUtils.writeStringToFile(file, StringUtils.join(estrutura, "\n"), "UTF-8");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return file;
+
 	}
 }
